81ea7dbc4ff6ba79656b070bda37f716542a7455
Zin <tyulkeds159357@gmail.com>
2021-05-15 17:24:35 +0800

    add mongoose config

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
+
+//引用config/mongoose
+require('./config/mongoose')
+
 //套入樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
