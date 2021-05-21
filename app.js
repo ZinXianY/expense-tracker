d0ef390ce9c9449dcdd7cafc57dbbdd16ec23cb4
Zin <tyulkeds159357@gmail.com>
2021-05-21 18:45:47 +0800

    add new routes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //載入工具
 const express = require('express')
 const exphbs = require('express-handlebars')
-const record = require('./models/record')
+const bodyParser = require('body-parser')
 const app = express()
 
 //引用 Record model
 const Record = require('./models/record')
-
+const Category = require('./models/category')
 
 //引用config/mongoose
 require('./config/mongoose')
@@ -14,6 +14,9 @@ require('./config/mongoose')
 //套入樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+//設定 body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //設定首頁路由
 app.get('/', (req, res) => {
@@ -23,6 +26,28 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//設計 new 頁面路由
+app.get('/new', (req, res) => {
+  Category.find()
+    .lean()
+    .then(category => res.render('new', { category }))
+    .catch(error => console.log(error))
+})
+
+//設定 Create 路由
+app.post('/records', (req, res) => {
+  const record = req.body
+  const category = req.body.category
+  Category.findOne({ name: category })
+    .lean()
+    .then(function (item) {
+      return (record.icon = item.icon)
+    })
+    .then(() => {
+      Record.create(record).then(() => res.redirect('/'))
+    })
+    .catch(error => console.log(error))
+})
 
 //設定監聽器
 app.listen(3000, () => {
