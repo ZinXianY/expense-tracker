c1c33edec750e9f23dc079648e6aa895232f47fc
Zin <tyulkeds159357@gmail.com>
2021-05-24 18:57:40 +0800

    modify models seeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,37 +1,12 @@
 const Category = require('../category')
 
-const RecordList = require('./record.json')
-
 const db = require('../../config/mongoose')
 
-
-const type = [
-  {
-    name: '家居物業',
-    icon: 'fas fa-home'
-  },
-  {
-    name: '交通出行',
-    icon: 'fas fa-shuttle-van'
-  },
-  {
-    name: '休閒娛樂',
-    icon: 'fas fa-grin-beam'
-  },
-  {
-    name: '餐飲食品',
-    icon: 'fas fa-utensils'
-  },
-  {
-    name: '其他',
-    icon: 'fas fa-pen'
-  }
-]
-
+const CategoryList = require('./record.json')
 
 //連線成功
 db.once('open', () => {
-  Category.create(type)
+  Category.create(CategoryList.categories)
     .then(() => {
       console.log('Category Done')
       return db.close()
