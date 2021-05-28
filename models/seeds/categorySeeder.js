62f6b7c6a0331a5fd77cc881c6918b60f39b1adb
Zin <tyulkeds159357@gmail.com>
2021-05-28 14:22:55 +0800

    modify models seeds

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,8 +1,8 @@
 const Category = require('../category')
 
-const db = require('../../config/mongoose')
-
 const CategoryList = require('./record.json')
+
+const db = require('../../config/mongoose')
 
 //連線成功
 db.once('open', () => {
