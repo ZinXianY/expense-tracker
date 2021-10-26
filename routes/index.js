1b36cc85ec232a8ea015fd2eb0924aa72d691325
Zin <tyulkeds159357@gmail.com>
2021-10-26 14:47:53 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,23 @@ const router = express.Router()
 //引入home 模組程式碼
 const home = require('./modules/home')
 
-//符合 / 字串導向 home 模組
-router.use('/', home)
-
 //引入 records 模組程式碼
 const records = require('./modules/records')
+
+//引入 users 模組程式碼
+const users = require('./modules/users')
+
+//引入 filter 模組程式碼
+const filter = require('../routes/modules/filter')
+
+//符合 / 字串導向 home 模組
+router.use('/', home)
 
 //符合 /records 字串導向 records 模組
 router.use('/records', records)
 
-//引入 filter 模組程式碼
-const filter = require('../routes/modules/filter')
+//符合 /users 字串導向 users 模組
+router.use('/users', users)
 
 //符合 /filter 字串導向 filter 模組
 router.use('/filter', filter)
