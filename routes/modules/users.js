8ff2fb2093cb1bbab469ee92ea88bd510fd4defd
Zin <tyulkeds159357@gmail.com>
2021-10-26 16:13:12 +0800

    add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
+//引用 User model
+const User = require('../../models/user')
+
 //設定登入路由
 router.get('/login', (req, res) => {
   res.render('login')
@@ -10,6 +13,34 @@ router.get('/login', (req, res) => {
 //設定註冊路由
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+//設定註冊功能
+router.post('/register', (req, res) => {
+  //取得註冊表單參數
+  const { name, email, password, confrimPassword } = req.body
+  //檢查是否註冊過
+  User.findOne({ email }).then(user => {
+    //如註冊過退回原本畫面
+    if (user) {
+      console.log('此信箱已被註冊!')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confrimPassword
+      })
+    } else {
+      //沒註冊過寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
 })
 //匯出路由
 module.exports = router
