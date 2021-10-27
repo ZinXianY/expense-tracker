2c5a17318c4aacdb5f1b4186dd25b9fe507db8a3
Zin <tyulkeds159357@gmail.com>
2021-10-27 15:43:17 +0800

    add user authentication in record and home routers 2

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
+//引入 middleware
+const { authenticator } = require('../middleware/auth')
+
 //引入home 模組程式碼
 const home = require('./modules/home')
 
@@ -14,14 +17,14 @@ const users = require('./modules/users')
 //引入 filter 模組程式碼
 const filter = require('../routes/modules/filter')
 
-//符合 / 字串導向 home 模組
-router.use('/', home)
-
 //符合 /records 字串導向 records 模組
-router.use('/records', records)
+router.use('/records', authenticator, records)
 
 //符合 /users 字串導向 users 模組
 router.use('/users', users)
+
+//符合 / 字串導向 home 模組
+router.use('/', authenticator, home)
 
 //符合 /filter 字串導向 filter 模組
 router.use('/filter', filter)
